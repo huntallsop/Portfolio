cd7fadbad9f8bd93615a0e65331826322d143628
huntallsop <huntallsop@gmail.com>
2025-11-02 16:18:36 -0700

    Revert "Add PastCompanies section under Hero (with PNG fallbacks)"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Hero from "./components/Hero/Hero";
-import PastCompanies from "./components/PastCompanies/PastCompanies";
 
 export default function App() {
   return (
     <main>
       <Hero />
-      <PastCompanies />
     </main>
   );
 }
