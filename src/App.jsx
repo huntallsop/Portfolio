05c249b196d74678bf84b7039d4e218093354235
huntallsop <huntallsop@gmail.com>
2025-11-02 16:12:02 -0700

    Add PastCompanies section with logo fallbacks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero/Hero";
+import PastCompanies from "./components/PastCompanies/PastCompanies";
 
 export default function App() {
   return (
     <main>
       <Hero />
+      <PastCompanies />
     </main>
   );
 }
