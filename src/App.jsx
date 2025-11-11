27389c9757d50dd3539dba7c7ad362db6cc31466
huntallsop <huntallsop@gmail.com>
2025-11-10 17:49:56 -0700

    Add loan servicing case study route and page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero/Hero";
 import PastCompanies from "./components/PastCompanies/PastCompanies";
 import Work from "./components/Work/Work";
 import Hello from "./components/Hello/Hello";
 import Footer from "./components/Footer/Footer";
+import LoanServicingCaseStudy from "./pages/LoanServicingCaseStudy";
 
-export default function App() {
+function HomePage() {
   return (
     <main>
       <Hero />
@@ -13,5 +15,19 @@ export default function App() {
       <Hello />
       <Footer />
     </main>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/loan-servicing-case-study"
+          element={<LoanServicingCaseStudy />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
